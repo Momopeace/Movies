41ca277553cb8ef546e0845bdcdd6e513f21be29
momopeace <[mohamedkadourparis@gmail.com*](mailto:mohamedkadourparis@gmail.com)>
2023-02-27 15:21:16 +0100

    fini

---
+++ MovieAmerican.java
@@ -0,0 +1,15 @@
+class MovieAmerican {
+
+    public static void main(String[] args) {
+       String nameMovie ="Indiana Jones and the last Crusade";
+        System.out.println(nameMovie);
+        
+    
+     int sortie = 1989;
+     System.out.println(sortie);
+     
+     double note = 3.14;
+     System.out.println(note);
+   }  
+    
+}
